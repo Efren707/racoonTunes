6706346160078fd2f3d4ffefb688e9f22964a2ca
efren <efrenbahen@gmail.com>
2021-10-29 15:12:47 -0500

    working on redering issues

---
+++ frontend/racoonTunes.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    debugger
+    ReactDOM.render(<h1>Welcome to RacoonTunes!</h1>, root);
+});
