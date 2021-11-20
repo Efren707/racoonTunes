de3c1b5dcd7d8dcd8e9874615c3d254348fc4dc2
efren <efrenbahen@gmail.com>
2021-11-20 15:53:47 -0600

    add songs to discover page

--- frontend/components/home/home_page_container.jsx
+++ frontend/components/home/home_page_container.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import {connect} from "react-redux";
 import {withRouter} from 'react-router-dom';
-import {createNewSong} from '../../actions/song_actions'
+import {createNewSong, receiveAllSongs} from '../../actions/song_actions';
+import SongIndexItem from "../songs/song_index_item";
 import GreetingContainer from '../greeting/greeting_container';
 
 class HomePageContainer extends React.Component {
 
+    componentDidMount(){
+        this.props.receiveAllSongs();
+    }
+
     render() {
 
+        if(!this.props.songs) return [];        
+        
+        const { songs } = this.props;
         
         if (this.props.pathname){
             return (
@@ -20,6 +28,15 @@ class HomePageContainer extends React.Component {
                    <div className="home-page-content">
                         <h2>Hear what’s trending for free in the RacoonTunes community</h2>
                    </div>
+
+                   <div className="discover-chart">
+
+                        <ul>
+                            {
+                                songs.slice(Math.max(songs.length - 5, 0)).map((song, index) => <SongIndexItem song={song} index={index} key={song.id} />)
+                            }
+                        </ul>
+                    </div>
 
                     <img className="main-picture" src={window.homeIphonePic}></img>
                     <img className="main-picture" src={window.homeSecondPic}></img>
@@ -35,11 +52,13 @@ class HomePageContainer extends React.Component {
 
 const mSTP = (state, ownProps) => ({
     currentUser: state.session.id,
+    songs: Object.values(state.entities.songs),
     pathname: ownProps.location.pathname === '/'
 })
 
 const mDTP = dispatch => ({
-    createSong: song => dispatch(createNewSong(song))
+    createSong: song => dispatch(createNewSong(song)),
+    receiveAllSongs: () => dispatch(receiveAllSongs())
 })
 
 export default withRouter(connect(mSTP, mDTP)(HomePageContainer))
