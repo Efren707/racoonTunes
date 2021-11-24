8230f2c856790bcb563472610f19141ea0872b79
efren <efrenbahen@gmail.com>
2021-11-23 18:25:44 -0600

    add song model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,4 +10,15 @@
 #  updated_at :datetime         not null
 #
 class Comment < ApplicationRecord
+    validates :body, :author_id, :song_id, presence: true
+
+    belongs_to :user,
+        primary_key: :id,
+        foreign_key: :author_id,
+        class_name: :User 
+
+    belongs_to :song, 
+        primary_key: :id,
+        foreign_key: :song_id,
+        class_name: :Song
 end
