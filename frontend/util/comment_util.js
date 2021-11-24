3cf5c5f08af4744aef54e4e2a6a9cd66dc4f3338
efren <efrenbahen@gmail.com>
2021-11-23 18:53:32 -0600

    add comment routes

---
+++ frontend/util/comment_util.js
@@ -0,0 +1,38 @@
+export const receiveComments = () => {
+    return $.ajax({
+        url: `/api/comments`,
+        method: 'GET'
+    })
+}
+
+export const receiveComment = commentId => {
+    return $.ajax({
+        url: `/api/comments/${commentId}`,
+        method: 'GET',
+        data: { commentId }
+    })
+}
+
+export const receiveSongComments = songId => {
+    return $.ajax({
+        url: `/api/songs/${songId}/comments`,
+        method: 'GET',
+        data: {songId}
+    })
+}
+
+export const createComment = comment => {
+    return $.ajax({
+        url: `/api/comments`,
+        method: 'POST',
+        data: comment,
+    })
+}
+
+export const deleteComment = commentId => {
+    return $.ajax({
+        url: `/api/comments/${commentId}`,
+        method: 'DELETE',
+        data: {commentId}
+    })
+}
