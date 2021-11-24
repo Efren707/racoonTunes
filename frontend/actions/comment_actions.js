412988ebfbdb6303511ccf64eaca4de7cccd66ee
efren <efrenbahen@gmail.com>
2021-11-23 19:28:24 -0600

    add comment actions and reducer

---
+++ frontend/actions/comment_actions.js
@@ -0,0 +1,45 @@
+import * as CommentApiUtil from '../util/comment_util';
+
+export const RECEIVE_COMMENTS = 'RECEIVE_COMMENTS';
+export const RECEIVE_COMMENT = 'RECEIVE_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+
+const receiveComments = comments => ({
+    type: RECEIVE_COMMENTS,
+    comments
+})
+
+const receiveComment = comment => ({
+    type: RECEIVE_COMMENT,
+    comment
+})
+
+const removeComment = commentId => ({
+    type: REMOVE_COMMENT,
+    commentId
+})
+
+export const receiveCurrentSongComments = songId => dispatch => {
+    return CommentApiUtil.receiveSongComments(songId)
+    .then(comms => dispatch(receiveComment(comms)))
+}
+
+export const receiveAllComments = () => dispatch => {
+    return CommentApiUtil.receiveComments()
+    .then(songs => dispatch(receiveComments(songs)))
+}
+
+export const receiveCurrentComment = commentId => dispatch => {
+    return CommentApiUtil.receiveComment(commentId)
+    .then(comm => dispatch(receiveComment(comm)))
+}
+
+export const createNewComment = comment => dispatch => {
+    return CommentApiUtil.createSong(comment)
+    .then(comm => dispatch(receiveComment(comm)))
+}
+
+export const removeCurrentComment = commentId => dispatch => {
+    return CommentApiUtil.deleteComment(commentId)
+    .then(comm => dispatch(removeComment(comm.id)))
+}
---
+++ frontend/reducers/comments_reducer.js
@@ -0,0 +1,22 @@
+import { RECEIVE_COMMENTS, RECEIVE_COMMENT, REMOVE_COMMENT } from "../actions/comment_actions";
+
+const commentsReducer = (oldState={}, action) => {
+    Object.freeze(oldState);
+    let nextState = Object.assign({}, oldState);
+
+    switch (action.type) {
+        case RECEIVE_COMMENTS:
+            return action.comments;
+        case RECEIVE_COMMENT:
+            
+            nextState[action.song.id] = action.comment;       
+            return nextState;
+        case REMOVE_COMMENT:
+            delete nextState[action.commentId];
+            return nextState;
+        default:
+            return oldState;
+    }
+}
+
+export default commentsReducer;
