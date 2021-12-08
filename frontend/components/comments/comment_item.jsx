aa93a6f5fcdcd5fa8a01cbaefb755cfb5b152038
efren <efrenbahen@gmail.com>
2021-12-08 15:31:10 -0600

    add comment delete button

--- frontend/components/comments/comment_item.jsx
+++ frontend/components/comments/comment_item.jsx
@@ -1,18 +1,47 @@
 import React from "react";
+import {connect} from 'react-redux';
+import { removeCurrentComment } from '../../actions/comment_actions';
 
-class CommentItem extends React.Component {
+class CommentIndexItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.deleteComment = this.deleteComment.bind(this);
+    }
+
+    deleteComment(){
+        this.props.removeComment(this.props.comment)
+    }
 
     render() {
-        const {comment} = this.props;
+        const {comment, currentUser} = this.props;
+
+        let deleteButton;
+
+        if(comment.author_id === currentUser.id){
+            deleteButton = <button onClick={this.deleteComment}>delete</button>
+        }
 
         return(
 
             <li>
                 <p>{comment.body}</p>
+                {deleteButton}
             </li>            
             
         )
     }
 }
 
+const mSTP = (state, ownProps) => {
+    return {
+        currentUser: state.entities.users[state.session.id]
+    }
+}
+
+const mDTP = dispatch => ({
+    removeComment: comment => dispatch(removeCurrentComment(comment.id))
+})
+
+const CommentItem = connect(mSTP, mDTP)(CommentIndexItem)
 export default CommentItem;
