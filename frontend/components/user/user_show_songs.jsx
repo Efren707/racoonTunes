8d9e0e84d47e5488e2b54a2bc82a97bfb261588d
efren <efrenbahen@gmail.com>
2021-12-10 02:50:26 -0600

    add favicon

--- frontend/components/user/user_show_songs.jsx
+++ frontend/components/user/user_show_songs.jsx
@@ -15,6 +15,7 @@ class UserSongIndexItem extends React.Component {
                     <div className="user-song-item-name">
                         <h2>{user.username}</h2>
                         <h1>{song.song_name}</h1>
+                        <audio controls className="song-show-audio" src={song.audio} type="audio/mpeg" />
                     </div>
                 </li> 
             </div>
