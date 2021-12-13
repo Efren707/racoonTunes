47415e3df646e0b5e5c4bf2ecb95d26cc711acf2
efren <efrenbahen@gmail.com>
2021-12-12 23:49:00 -0600

    add user edit form

--- frontend/components/user/user_edit.jsx
+++ frontend/components/user/user_edit.jsx
@@ -7,10 +7,20 @@ class EditUserForm extends React.Component {
 
     constructor(props) {
         super(props);  
-        
+        this.state = ({...this.props.user})
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    update(field){
+        return e => (
+            this.setState({[field]: e.currentTarget.value})
+        )
+    }
     
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.updateUser(this.state);
+    }
 
     render(){
         
@@ -24,7 +34,24 @@ class EditUserForm extends React.Component {
                     <br/>
                     <h1>Update Profile</h1>
                     <br/>
-                    
+                    <label><span>Name *</span>&nbsp;
+                        <input value={this.state.name} onChange={this.update("name")}></input>
+                    </label>
+                    <br/>
+                    <br/>
+
+                    <label><span>Username *</span>&nbsp;
+                        <input value={this.state.username} onChange={this.update("username")}></input>
+                    </label>    
+                    <br/>
+                    <br/>
+
+                    <label><span>Email *</span>&nbsp;
+                        <input value={this.state.email} onChange={this.update("email")}></input>
+                    </label>          
+                    <br/>
+                    <br/>
+                    <button  className="edit-button" onClick={this.handleSubmit}>Update</button>
 
                 </form>
                 </div>
