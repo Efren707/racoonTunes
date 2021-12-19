039a3a50646cb1060857c873a165ae86d5af96b2
efren <efrenbahen@gmail.com>
2021-12-19 13:52:02 -0600

    add tyler user

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,6 +39,14 @@ weeknd = User.create!({username: 'TheWeeknd', password: '123456', name: 'The Wee
 weeknd_pic = open("https://racoontunes-seeds.s3.us-west-1.amazonaws.com/frankProfile.png")
 weeknd.profile_pic.attach(io: weeknd_pic, filename: "frankProfile.png")
 
+tyler = User.create!({username: 'Tyler', password: '123456', name: 'Tyler The Creator', email: 'ofwgkta@user.com'})
+tyler_pic = open("https://racoontunes-seeds.s3.us-west-1.amazonaws.com/frankProfile.png")
+tyler.profile_pic.attach(io: tyler_pic, filename: "frankProfile.png")
+
+weeknd = User.create!({username: 'TheWeeknd', password: '123456', name: 'The Weeknd', email: 'starboy@user.com'})
+weeknd_pic = open("https://racoontunes-seeds.s3.us-west-1.amazonaws.com/frankProfile.png")
+weeknd.profile_pic.attach(io: weeknd_pic, filename: "frankProfile.png")
+
 song1 = Song.create!({song_name: 'Stargazing', genre: "Hip-Hop & Rap", description: "In the song, Travis Scott sings about how overcoming his past ways and abstinence from lean has helped him become a better influence", author_id: travis.id})
 song1_pic = open("https://racoontunes-seeds.s3.us-west-1.amazonaws.com/astroworld.jpg")
 song1.photo.attach(io: song1_pic, filename: "astroworld.jpg")
