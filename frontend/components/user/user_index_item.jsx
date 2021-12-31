88950e2dbc2392f27cf9e2b95a69808f17d16ccc
efren <efrenbahen@gmail.com>
2021-12-31 16:06:06 -0600

    refactored discover artist comp

--- frontend/components/user/user_index_item.jsx
+++ frontend/components/user/user_index_item.jsx
@@ -14,7 +14,10 @@ class UserIndexItem extends React.Component {
 
             <li className="discover-users-li">
                 <Link to={`/users/${user.id}`}><img className="discover-user-pic" src={user.profile_pic} alt="user profile photo" /></Link>
-                <Link to={`/users/${user.id}`} className="discover-user-username"><h2>{user.username}</h2></Link>
+                <div className="discover-user-names">
+                    <Link to={`/users/${user.id}`} className="discover-user-username"><h2>{user.username}</h2></Link>
+                    <h3>{user.name}</h3>
+                </div>
             </li>            
             
         )
