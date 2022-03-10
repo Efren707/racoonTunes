934e3e3e61668b5e7a233e7a3a514b9801f34141
efren <efrenbahen@gmail.com>
2022-03-10 14:23:58 -0600

    search bar filters songs

--- frontend/components/nav/searchbar.jsx
+++ frontend/components/nav/searchbar.jsx
@@ -6,18 +6,39 @@ class SearchBar extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = ({
+            songSearch: ""
+        })
     } 
 
     componentDidMount(){
         this.props.receiveAllSongs();
     }
 
+    update(field){
+        return e => (
+            this.setState({[field]: e.currentTarget.value})
+        )
+    }
+
     render(){
         if(!this.props.songs) return [];
-        
+
+        let searched = [];
+
+        if(this.state.songSearch.length > 0){
+            this.props.songs.forEach(song => {
+                if((song.song_name.toUpperCase() === this.state.songSearch.toUpperCase()) || (song.song_name.toUpperCase().includes(this.state.songSearch.toUpperCase()))){
+                    searched.push(song)
+                }
+            })
+        }
+
+        console.log(searched)
+
         return (
             <div>
-                <input type="text" className="nav-search-bar" placeholder="Coming soon..."></input>
+                <input type="text" className="nav-search-bar" onChange={this.update('songSearch')} placeholder="Search"></input>
             </div>
         )
     }
